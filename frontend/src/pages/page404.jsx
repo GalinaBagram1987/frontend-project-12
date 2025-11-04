27e11c3e32745472c917cfc07cf1abad60194cfc
GalinaBagram1987 <gaby_ev@mail.ru>
2025-11-04 22:09:12 +1000

    add 404

--- frontend/src/pages/page404.jsx
+++ frontend/src/pages/page404.jsx
@@ -1,8 +1,9 @@
 const Page404 = () => {
 return (
-  <div className='text-center flex-grow-1 d-flex flex-column justify-content-center'>
+  <div className='text-center d-flex flex-column justify-content-center flex-grow-1'>
     <img alt='Страница не найдена'
-      className='img-fluid h-25'
+      className='img-fluid'
+      style={{ height: '25vh' }}
       src='/assets/images/404_slack.svg' />
       <h1 className='h4 text-muted'>Страница не найдена</h1>
       <p className='text-muted'>
