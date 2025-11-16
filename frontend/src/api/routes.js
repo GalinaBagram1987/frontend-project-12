066793b2f34af72de088be11e438bd247aa8f448
GalinaBagram1987 <gaby_ev@mail.ru>
2025-11-16 22:45:27 +1000

    add routes

--- frontend/src/api/routes.js
+++ frontend/src/api/routes.js
@@ -1,3 +1,3 @@
-export const baseUrl = import.meta.env.PROD 
-? 'https://testslackbagram.onrender.com' // Production URL
-: 'http://localhost:5001'; // Development URL
+export const baseUrl = import.meta.env.PROD
+  ? 'https://testslackbagram.onrender.com' // Production URL
+  : 'http://localhost:5001'; // Development URL
