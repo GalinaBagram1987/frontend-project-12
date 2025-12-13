2bc804796e710902a269b4cbc20e070b0ef65e53
GalinaBagram1987 <gaby_ev@mail.ru>
2025-12-13 21:25:22 +1000

    change server.js6

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const HEXLET_API =
     ? 'http://localhost:5000' // локально в том же контейнере
     : 'http://localhost:5001'; // в development
 
-app.use('/api/v1/*', async (req, res) => {
+app.use(/^\/api\/v1\/(.*)/, async (req, res) => {
   try {
     const remainingPath = req.params[0] || '';
     // const url = `${HEXLET_API}/api/v1/${req.params.path}`;
