785abfa9428bf0d78141ea8a1a71663df69121ad
GalinaBagram1987 <gaby_ev@mail.ru>
2025-12-13 23:28:49 +1000

    change server.js11

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const { spawn } = require('child_process');
+const axios = require('axios');
 
 const app = express();
 
