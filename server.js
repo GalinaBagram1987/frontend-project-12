69d8e09a66b5d7dce21003f832892f1b97044cb6
GalinaBagram1987 <gaby_ev@mail.ru>
2025-12-15 13:28:57 +1000

    correct server cors and socket

--- server.js
+++ server.js
@@ -11,15 +11,16 @@ const PORT = process.env.PORT || 5001;
 
 // ------ЗАПУСК HEXLET СЕРВЕРА В ФОНЕ-------
 let hexletServer;
+const HEXLET_PORT = 5000;
 
 if (process.env.NODE_ENV === 'production') {
   // Запускаем Hexlet сервер как дочерний процесс
-  hexletServer = spawn('npx', ['start-server', '--port', '5000'], {
+  hexletServer = spawn('npx', ['start-server', '--port', HEXLET_PORT], {
     stdio: 'inherit', // вывод hexletServer будет отображаться в консоли процесса-родителя
     detached: true, ///отделить дочерний процесс от родительского, чтобы он продолжал работать даже после того, как родительский процесс завершится.
   });
 
-  console.log('Hexlet server started on port 5000');
+  console.log(`Hexlet server started on port ${HEXLET_PORT}`);
 
   // При завершении основного процесса убиваем Hexlet сервер
   process.on('exit', () => {
@@ -28,6 +29,32 @@ if (process.env.NODE_ENV === 'production') {
     }
   });
 }
+
+// ------ CORS для всех сред ------
+const cors = require('cors');
+const allowedOrigins = process.env.NODE_ENV === 'production' // разрешенные данные
+  ? ['https://testslack2bagram.onrender.com']
+  : ['http://localhost:5002'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log('CORS blocked origin:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
+
+app.use(express.json());
+
+
+
+
+
 
 // ------ CORS ТОЛЬКО ДЛЯ DEVELOPMENT ------
 // if (process.env.NODE_ENV !== 'production') {
@@ -48,7 +75,6 @@ if (process.env.NODE_ENV === 'production') {
 //   );
 // }
 
-app.use(express.json());
 
 // ------- ПРОКСИ ДЛЯ SOCKET.IO ------
 const socketProxy = createProxyMiddleware('/socket.io', {
