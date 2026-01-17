8b3e17cb5d7037a63f34379de0a127e926280265
GalinaBagram1987 <gaby_ev@mail.ru>
2026-01-17 19:34:41 +1000

    add 3 symbols in validate login password

--- frontend/src/library/yup/loginValidate.js
+++ frontend/src/library/yup/loginValidate.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 const loginSchema = (t) => {
   return yup.object().shape({
   username: yup.string().min(3, t('validationError.nameLength')).required(t('login.nameLogin')),
-  password: yup.string().min(6, t('validationError.passwordLength')).required(t('validationError.requiredPassword')),
+  password: yup.string().min(3, t('validationError.passwordLength')).required(t('validationError.requiredPassword')),
 });
 };
 
